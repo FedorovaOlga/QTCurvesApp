03285ac1144b5a6f8ababc67c41d9492d4ddd908
Olga <olga-ola1997@mail.ru>
2021-09-03 22:09:17 +0200

    Creating basic shapes with options of scaling, changing step count and internal length. Adding the option of background and line color.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,15 +1,94 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QColorDialog>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    this->ui->spinScale->setValue(this->ui->renderArea->scale());
+    this->ui->spinLength->setValue(this->ui->renderArea->internalLength());
+    this->ui->spinBox->setValue(this->ui->renderArea->stepCount());
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::update_ui()
+{
+    this->ui->spinScale->setValue(this->ui->renderArea->scale());
+    this->ui->spinLength->setValue(this->ui->renderArea->internalLength());
+    this->ui->spinBox->setValue(this->ui->renderArea->stepCount());
+}
+
+
+void MainWindow::on_btnAstroid_clicked()
+{
+    this->ui->renderArea->setShape(RenderArea::Astroid);
+    this->ui->renderArea->repaint();
+    update_ui();
+
+}
+
+
+void MainWindow::on_btnCicloid_clicked()
+{
+    this->ui->renderArea->setShape(RenderArea::Cycloid);
+    this->ui->renderArea->repaint();
+    update_ui();
+}
+
+
+void MainWindow::on_btnHuygens_clicked()
+{
+    this->ui->renderArea->setShape(RenderArea::HuygensCycloid);
+    this->ui->renderArea->repaint();
+    update_ui();
+}
+
+
+void MainWindow::on_btnHypoCicloid_clicked()
+{
+    this->ui->renderArea->setShape(RenderArea::HypoCycloid);
+    this->ui->renderArea->repaint();
+    update_ui();
+}
+
+
+void MainWindow::on_btnBGColor_clicked()
+{
+    QColor color = QColorDialog::getColor(this->ui->renderArea->backgroundColor(), this, "Select Color");
+    this->ui->renderArea->setBackgroundColor(color);
+}
+
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    QColor color = QColorDialog::getColor(this->ui->renderArea->shapeColor(), this, "Select Color");
+    this->ui->renderArea->setShapeColor(color);
+}
+
+
+void MainWindow::on_spinScale_valueChanged(double scale)
+{
+    this->ui->renderArea->setScale(scale);
+}
+void MainWindow::on_Length_valueChanged(double intLenth)
+{
+    this->ui->renderArea->setInternalLength(intLenth);
+}
+
+void MainWindow::on_spinLength_valueChanged(double intLenth)
+{
+    this->ui->renderArea->setInternalLength(intLenth);
+}
+
+
+void MainWindow::on_spinBox_valueChanged(int cnt)
+{
+    this->ui->renderArea->setStepCount(cnt);
 }
 
--- mainwindow.h
+++ mainwindow.h
@@ -15,7 +15,29 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_btnAstroid_clicked();
+
+    void on_btnCicloid_clicked();
+
+    void on_btnHuygens_clicked();
+
+    void on_btnHypoCicloid_clicked();
+
+    void on_btnBGColor_clicked();
+
+    void on_pushButton_2_clicked();
+
+    void on_spinScale_valueChanged(double arg1);
+
+    void on_Length_valueChanged(double arg1);
+
+    void on_spinLength_valueChanged(double arg1);
+
+    void on_spinBox_valueChanged(int arg1);
+
 private:
     Ui::MainWindow *ui;
+    void update_ui();
 };
 #endif // MAINWINDOW_H
